bde960f4ba48cec5f0eea33b0f291a984c4af821
Deep Narayan <dnarayanchaurasiya@gmail.com>
2023-07-23 10:13:02 +0530

    1st web age commit

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 # create a new flask based application
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 # create flask app
 app = Flask(__name__) #entry point for Flask
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return "Hello World"
 
 @app.route('/welcome')
 def welcome():
     return 'Welcome!'
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    #return redirect(url_for('index.html'))
 if __name__ == '__main__':
     app.run()
