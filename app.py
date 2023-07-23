f66a35a4663e7aae2abdaaad43faee943823db97
ideeep <52308265+ideeep@users.noreply.github.com>
2023-07-23 12:00:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,65 @@
-# create a new flask based application
-from flask import Flask, render_template, url_for, redirect
+## Create a simple flask application
 
-# create flask app
-app = Flask(__name__) #entry point for Flask
+from flask import Flask,render_template,request,redirect,url_for
+
+## create the flask app
+
+app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return "<h2>Hello, World!</h2>"
 
 @app.route('/welcome')
 def welcome():
-    return 'Welcome!'
+    return "Welcome to the Flask Tutorials"
 
 @app.route('/index')
 def index():
     return render_template('index.html')
-    #return redirect(url_for('index.html'))
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person is passed and the score is "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed and the score is "+str(score)
+
+
+@app.route('/calculate',methods=['POST','GET'])
+def calculate():
+    if request.method=='GET':
+        return render_template('calculate.html')
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        # dictionary
+        disct1 = {'math': maths, 'science': science, history: history}
+        print(disct1)
+        for x, y in disct1.items():
+            print('deep', x, y)
+        #end dictionary
+        average_marks=(maths+science+history)/3
+        result="" 
+        if average_marks>=50:
+            result="success"
+        else:
+            result="fail"
+
+        #return redirect(url_for(result,score=average_marks))
+
+
+        return render_template('result.html',results=average_marks, disct1=disct1)
+
+
+## Assignemnt Try for loop
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
